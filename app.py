e2ad351d2d336a5cc263cb4e279f5837b900bc48
candice 吴 <candicewu@candices-MacBook-Pro.local>
2025-04-26 15:30:44 -0400

    2nd

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
-
+from matcher import RideRequest, perfect_match, potential_match
+from datetime import datetime
 existing_requests = []
+
+def main()
+    while True:
+        name = input("Name")
+        email = input("Email: ")
+        airport = input("Airport (e.g., JFK, LGA):")
+        datetime_str = input("Time (YYYY/MM/DD HH:MM)")
+        dt = datetime.strptime(datetime_str, "%Y-%m-%d %H: %M")
+
+        new_req = RideRequest(name, email, airport, dt)
+
+        matches = perfect_match(new_req, existing_requests)
+        if matches:
+            for m in matches:
+                print("f - {m.name}" at {m.datetime}")
