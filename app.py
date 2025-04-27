e58456a3a6ceedb32bcd775928eda044ba8065a4
candice 吴 <candicewu@candices-MacBook-Pro.local>
2025-04-26 23:29:40 -0400

    sqlite testing

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from datetime import datetime
 existing_requests = []
 
 def main():
+    print("start!")
+    session = Session()
+
     while True:
         name = input("Name: ")
         email = input("Email: ")
@@ -11,19 +14,41 @@ def main():
         dt = datetime.strptime(datetime_str, "%Y/%m/%d %H:%M")
 
         new_req = Riderequest(name, email, airport, dt)
-
-        matches = perfect_match(new_req, existing_requests)
-        if matches:
-            for m in matches:
-                print(f" - {m.name} at {m.datetime}")
-        else:
-            pot = potential_match(new_req, existing_requests)
-            if pot:
-                for m in pot:
-                    print(f"-potentially with {m.name} at {m.datetime}")
-            else:
-                existing_requests.append(new_req)
         
+        session.add(new_req)
+        session.commit()
+        print("req saved")
+
+        total_requests = session.query(Riderequest).count()
+        print(f"total req saved: {total_requests}")
+
+        if total_requests >= 10:
+            print("matching starts...")
+
+            all_reqs = session.query(Riderequest).all()
+            
+            matches = perfect_match(new_req, all_reqs)
+
+            if matches:
+                for m in matches:
+                    print(f" match with {m.name} at {m.datetime}")
+                    session.delete(m)
+                session.delete(new_req)
+                session.commit()
+            else:
+                pot = potential_match(new_req, all_reqs)
+                if pot:
+                    for m in pot:
+                        print(f"-potentially with {m.name} at {m.datetime}")
+                        
+                else:
+                    print(f"no match now")
+        cont = input("\n add? (y/n): ")
+        if cont() != 'y':
+            break
+
+    session.close()
+    print("end of matching")
     
 if __name__ == "__main__":
     main()
