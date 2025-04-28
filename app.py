3828a52068003a91a975fe68b4ef671f4371e73d
candice 吴 <candicewu@candices-MacBook-Pro.local>
2025-04-27 22:55:00 -0400

    milestone 1

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ existing_requests = []
 def main():
     print("start!")
     session = Session()
+    ready = False
 
     while True:
         name = input("Name: ")
@@ -23,27 +24,38 @@ def main():
         total_requests = session.query(Riderequest).count()
         print(f"total req saved: {total_requests}")
 
-        if total_requests >= 10:
+        all_reqs = session.query(Riderequest).all()
+        if not ready:
+            if total_requests >= 10:
+                ready = True
+            else: 
+                print (f"waiting for more requests")
+                continue
+        
+
+        
             print("matching starts...")
-
-            all_reqs = session.query(Riderequest).all()
             
-            matches = perfect_match(new_req, all_reqs)
+        all_reqs = session.query(Riderequest).all()
+        
+        matches = perfect_match(new_req, all_reqs)
+        #potential_matches = potential_match(new_req, all_reqs)
 
-            if matches:
-                for m in matches:
-                    print(f" match with {m.name} at {m.datetime}")
-                    session.delete(m)
-                session.delete(new_req)
-                session.commit()
+        if matches:
+            for m in matches:
+                print(f'"{m[0].name}" at {m[0].datetime} matches with {m[1].name} at {m[1].datetime}"')
+                session.delete(m[0])
+                session.delete(m[1])
+            session.delete(new_req)
+            session.commit()
+        else:
+            pot = potential_match(new_req, all_reqs)
+            if pot:
+                for m in pot:
+                    print(f"-potentially with {m.name} at {m.datetime}")
             else:
-                pot = potential_match(new_req, all_reqs)
-                if pot:
-                    for m in pot:
-                        print(f"-potentially with {m.name} at {m.datetime}")
-                        
-                else:
-                    print(f"no match now")
+                print(f"no match now")
+
         cont = input("\n add? (y/n): ")
         if cont != 'y':
             break
