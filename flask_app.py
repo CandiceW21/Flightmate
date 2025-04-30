df99e4dcf8ad957af17a448ea84d26b91158d766
candice 吴 <candicewu@candices-MacBook-Pro.local>
2025-04-30 17:22:06 -0400

    1722

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ from datamodel import Base, Riderequest, perfect_match, potential_match, Session
 from datetime import datetime
 
 app = Flask(__name__)
+ready = False
 
 @app.route('/', methods = ['GET','POST'])
 def index():
@@ -18,7 +19,35 @@ def index():
         session.add(new_req)
         session.commit()
 
-        return redirect('/')
-    requests = session.query(Riderequest).all()
-    return render_template('index.html', requests = requests)
+        total_requests = session.query(Riderequest).count()
+
+        if not ready and total_requests >= 10:
+            ready = True
+        
+        if ready: 
+            all_reqs = session.query(Riderequest).all()
+            matches = perfect_match(new_req, all_reqs)
+
+            if matches:
+                for m in matches:
+                    print(f'"{m[0].name}" at {m[0].datetime} matches with {m[1].name} at {m[1].datetime}"')
+                    session.delete(m[0])
+                    session.delete(m[1])
+                session.delete(new_req)
+                session.commit()
+            else:
+                pot = potential_match(new_req, all_reqs)
+                if pot:
+                    for m in pot:
+                        print(f"-potentially with {m.name} at {m.datetime}")
+                else:
+                    print(f"no match now")
+    return render_template("index.html")
+
+        
+
+           
+        
+        
+    
 
